2ed4397ef1d05092966a3715444810379475c7fe
jamesbanegas604-blip <jamesbanegas604@gmail.com>
2025-11-21 13:03:58 -0600

    buenas tardes jovenes

---
+++ src/lab6p1_jamesortiz/Lab6p1_jamesortiz.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab6p1_jamesortiz;
+
+/**
+ *
+ * @author james
+ */
+public class Lab6p1_jamesortiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
